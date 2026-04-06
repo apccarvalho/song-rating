1c5b9664effacb887f6d49e95f604e5e55a2dd16
apccarvalho <andreiacarvalho.apc@gmail.com>
2026-04-06 10:07:38 -0300

    Adição dos métodos create, edit, delete no SongController.

--- src/main/java/com/iftm/song_rating/controller/SongController.java
+++ src/main/java/com/iftm/song_rating/controller/SongController.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.iftm.song_rating.model.Song;
 import com.iftm.song_rating.service.SongService;
 
 @Controller
@@ -18,5 +22,32 @@ public class SongController {
 		model.addAttribute("songsList", songService.getAllSongs());
 		return "song/index";
 	}
+	
+	@GetMapping("/song/create")
+	public String create(Model model) {
+		model.addAttribute("song", new Song());
+		return "product/create";
+	}
+	
+	@PostMapping("/song/save")
+	public String postMethodName(@ModelAttribute("song") Song song){
+		songService.saveSong(song);
+		return "redirect:/song";
+	}
+	
+	@GetMapping("/song/delete/{id}")
+	public String delete(@PathVariable Long id) {
+		this.songService.deleteSongById(id);
+		return "redirect:/song";
+	}
+	
+	@GetMapping("/song/edit/{id}")
+	public String edit(@PathVariable Long id, Model model) {
+		Song song = songService.getSongById(id);
+		model.addAttribute("song", song);
+		return "song/edit";
+	}
+	
+	
 	
 }
