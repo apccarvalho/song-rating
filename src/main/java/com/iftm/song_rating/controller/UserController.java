16e152e582bf84805d43e8f648bc04c03a4a8f3d
apccarvalho <andreiacarvalho.apc@gmail.com>
2026-05-17 14:32:44 -0300

    Atualizacao das classes do usuario

--- src/main/java/com/iftm/song_rating/controller/UserController.java
+++ src/main/java/com/iftm/song_rating/controller/UserController.java
@@ -34,7 +34,7 @@ public class UserController {
 	
 	@GetMapping("/accesDenied")
 	public String getAccersDeniedPage() {
-		return "user/accersDeniedPage";
+		return "user/accessDeniedPage";
 	}
 	
 	
